aabe6985f97fb2fd00300b267ba1df85e559c568
hwangsuyun@naver.com <hwangsuyun@naver.com>
2022-09-02 22:08:52 +0900

    test_ipv6 (2022.09.02 syhwang)

---
+++ test_ipv6/client.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(int argc, char **argv)
+{
+    struct sockaddr_in6 svrsin6;
+    struct hostent *hp;
+    char ipv6_addr[16];
+    char addr6_str[40];
+    char buf[256];
+
+    int sockfd = 0;
+    int clilen = 0;
+
+    if (argc < 2)
+    {
+        printf("argv[1] is port number\n");
+        return -1;
+    }
+
+    sockfd = socket(AF_INET6, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        printf("socket create error\n");
+        return -1;
+    }
+
+    svrsin6.sin6_family   = AF_INET6;
+    svrsin6.sin6_flowinfo = 0;
+    svrsin6.sin6_port     = htons(atoi(argv[1]));
+
+    //inet_pton(AF_INET6, "3ffe:ffff:0:f101::1", (void *)&ipv6_addr);
+    inet_pton(AF_INET6, "::1", (void *)&ipv6_addr);
+    // in6addr_loopback 는 loopback 주소로 연결하기 위해서 
+    // 사용한다.  
+    // ipv6 에서의 loopbak 주소는 "::1" 이다.  
+    // 루프백주소로 연결하길 원한다면
+    // svrsin6.sin6_addr      = in6addr_loopback;   
+    // 혹은
+    // inet_pton(AF_INET6, "::1", (void *)&ipv6_addr); 
+    // 하면된다. 
+    memcpy((void *)&svrsin6.sin6_addr, (void *)&ipv6_addr, 16);
+    inet_ntop(AF_INET6, (void *)&svrsin6.sin6_addr, addr6_str, 40);
+
+    printf("%s\n", addr6_str);
+    printf("connect...\n");
+    clilen = sizeof(svrsin6);
+    if(connect(sockfd, (struct sockaddr *)&svrsin6, clilen) < 0)
+    {
+        printf("connect error\n");
+        return -1;
+    }
+    memset(buf, 0x00, 256);
+    read(0, buf, 256);
+    write(sockfd, buf, 256);
+    read(sockfd, buf, 256);
+    printf("-->%s", buf);
+
+    printf("Connect Success\n");
+    close(sockfd);
+
+    return 0;
+}
